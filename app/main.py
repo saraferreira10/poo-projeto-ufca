4ef1701fb48afc5401dabf73e452bd74c26d3d57
Sara Ferreira <sara.f.araujo12@gmail.com>
2025-12-16 17:37:40 -0300

    chore: database setup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.db.database import create_db_and_tables
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(title="The Catalog API", lifespan=lifespan)
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return {"status": "API rodando 🚀"}
