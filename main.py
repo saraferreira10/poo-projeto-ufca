50e7f86ca27843cde4a2d0bf39ab6e3ab62c20d4
Sara Ferreira <sara.f.araujo12@gmail.com>
2025-12-17 16:09:56 -0300

    feat(media): cleaned main.py

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
-from src.enums.enums import Classificacao, Genero
-from src.models.filme import Filme
+
+
+
+
+from src.db.dados import criar_tabelas
 
 
 def main():
-    # criar_tabelas()
-
-    try:
-        print("--- Iniciando Cadastro de Mídia ---")
-
-        m1 = Filme(
-            titulo="Inception",
-            genero=Genero.FICCAO_CIENTIFICA,
-            ano=2010,
-            duracao=148,
-            classificacao=Classificacao.C14,
-            elenco="Leo DiCaprio",
-        )
-
-        print(m1)
-        m1.iniciar_visualizacao()
-        m1.avaliar(9.5)
-        print(m1)
-
-    except ValueError as e:
-        print(f"\n❌ ERRO DE VALIDAÇÃO: {e}")
-
-    except Exception as e:
-        print(f"\n❗ OCORREU UM ERRO: {e}")
-
-    finally:
-        print("\n--- Operação Finalizada ---")
-
+    criar_tabelas()  # Inicializa o banco/SQLITE
 
 if __name__ == "__main__":
     main()
