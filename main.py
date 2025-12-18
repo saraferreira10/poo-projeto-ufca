e2bf8b40f77fa4cbea0a7799e019cf3a3b61f181
Holivane <holivane@gmail.com>
2025-12-18 18:09:46 -0300

    feat: adiciona mensagens de inicialização do CLI

--- main.py
+++ main.py
@@ -5,8 +5,13 @@
 from src.db.dados import criar_tabelas
 
 
+
 def main():
+    print("🎬 Sistema de Catálogo de Mídias - CLI Iniciado")
+    
     criar_tabelas()  # Inicializa o banco/SQLITE
+    
+    print("✅ Sistema inicializado com sucesso!\n")
 
 if __name__ == "__main__":
     main()
