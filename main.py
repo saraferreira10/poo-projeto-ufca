f44d656581c4ea08cb6091516db8e1a6f079c367
Holivane <holivane@gmail.com>
2025-12-18 19:58:10 -0300

    feat: encerramento do sistema

--- main.py
+++ main.py
@@ -9,5 +9,18 @@ def main():
     print("✅ Banco de dados SQLite inicializado com sucesso!")
     print("✅ Sistema inicializado com sucesso!\n")
 
+    while True:
+        print("\n=== MENU === \n")
+        print("-> Digite 'Sair' para encerrar o sistema ") 
+
+        choice = input().strip().lower()
+
+        if choice == "0" or choice == "sair":
+            print("\nEncerrando o sistema... \nSistema encerrado \n \n")
+            break
+        else:
+            print("!!! Opção inválida :( \n Tente novamente")
+
+
 if __name__ == "__main__":
     main()
