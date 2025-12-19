69cf7fa9db0cd3a54ceb90e2c90f45779d937cde
Holivane <holivane@gmail.com>
2025-12-18 21:15:41 -0300

    feat: Implementação da Interface para exibir o menu principal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.db.dados import criar_tabelas
+from src.utils.interface import Interface
 
 
 def main():
@@ -10,14 +11,17 @@ def main():
     print("✅ Sistema inicializado com sucesso!\n")
 
     while True:
-        print("\n=== MENU === \n")
-        print("-> Digite 'Sair' para encerrar o sistema ") 
+        Interface.exibir_menu_principal()
 
-        choice = input().strip().lower()
+        choice = input("\n").strip().lower()
 
-        if choice == "0" or choice == "sair":
+        if choice == "0":
             print("\nEncerrando o sistema... \nSistema encerrado \n \n")
             break
+        elif choice == "1":
+            print("-" * 80) 
+            print("Chamar a função que lista o que está cadastrado no catálogo") 
+            print("-" * 80) 
         else:
             print("!!! Opção inválida :( \n Tente novamente")
 
