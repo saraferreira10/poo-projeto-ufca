fbcc181b9ba9eea398ebfa85149ce84926a8ce4f
Holivane <holivane@gmail.com>
2025-12-18 21:14:01 -0300

    feat: criada pasta utils e classe Interface, responsável pelos prints na tela

---
+++ src/utils/interface.py
@@ -0,0 +1,8 @@
+class Interface:
+    def exibir_menu_principal():
+        print("\n" + "="*40)
+        print("      MENU PRINCIPAL")
+        print("="*40)
+        print(" [1] Ver Catálogo")
+        print(" [0] Sair do Sistema")
+        print("-" * 40)
